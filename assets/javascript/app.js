56daa15996a3a0ff837b84562dec6cb957329520
Nathaniel Jansen <nathanieljansen@Nathaniels-MacBook-Pro.local>
2018-03-01 07:00:02 -0700

    timer works and other changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,38 +2,59 @@
 var correctAnswers = 0;
 var wrongAnswers = 0;
 var notAnswered = 0;
+var userPick
+var counter = 5;
 
-var questions = ["What does this do?", "What about this?"]
-var randomQuestion = questions[Math.floor(Math.random() * questions.length)];
-var answer = []
+var questionsArr = [
+  {
+    question: "who is president?",
+    answers: ["Bill Maher", "Obama", "Brittany Spears", "Trump"],
+    correctAnswer: 3
+  },
 
-// Do I create a variable for 5 random questions?
-// 
-// List of questions with 4 answers, 3 incorrect, 1 correct
-// 
+  {
+    question: "what does this do?",
+    answers: ["Nothing", "Something", "Haha", "Right"],
+    correctAnswer: 1
+  }
+]
+var randomQuestion = questionsArr[Math.floor(Math.random() * questionsArr.length)];
 
 
 $(function () {
 
-  $(".startGame").html("Click Here to Start the Game!");
-  $(".question").html(" ");
-  $(".answer1").html(" ");
-  $(".answer2").html(" ");
-  $(".answer3").html(" ");
-  $(".answer4").html(" ");
-  $(".startGame, .answer1, .answer2, .answer3, .answer4").hover(function (e) {
+  $(".question").html("Click Here to Start the Game!");
+  $(".question").hover(function (e) {
     $(this).css("background-color", e.type === "mouseenter" ? "#d58ab2" : "transparent")
   })
+  $(".footer").hide();
 
-  $(".startGame").on("click", function () {
-    console.log(randomQuestion)
-    $(this).hide();
-    $(".question").html(randomQuestion);
-    $(".answer1").html("Answer 1");
-    $(".answer2").html("Answer 2");
-    $(".answer3").html("Answer 3");
-    $(".answer4").html("Answer 4");
+  $(".question").on("click", function () {
+    $(this).unbind();
+    $(this).css("background-color", "#72f7fa")
+    $(".footer").show();
+    timer()
+    $(".question").html(randomQuestion.question);
+    var answers = [];
+    var theseAnswers = randomQuestion.answers;
+    for (var i = 0; i < theseAnswers.length; i++) {
+      answers.push('<p class=" taco col-md-6 col-sm-6 col-6" data-id' + [i] + '>' + theseAnswers[i] + '</p>');
+
+    }
+    $(".answersDiv").html(answers);
+  })
+
+
+  function timer() {
+    var interval = setInterval(function () {
+      counter--;
+      console.log(counter)
+      $(".countDown").html(counter)
+      if (counter === 0) {
+        $(".countDown").html("You Lost")
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+
 });
-
-});
-
